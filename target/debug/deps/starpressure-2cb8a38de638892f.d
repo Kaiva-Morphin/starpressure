d17d9b064178d6e384028ae9237af18137d40eec
Kaiva-Morphin <bebraeveryday0@gmail.com>
2024-02-22 18:12:38 +0300

    bebra!

---
+++ target/debug/deps/starpressure-2cb8a38de638892f.d
@@ -0,0 +1,5 @@
+C:\prog\rust\starpressure\target\debug\deps\libstarpressure-2cb8a38de638892f.rmeta: src\main.rs
+
+C:\prog\rust\starpressure\target\debug\deps\starpressure-2cb8a38de638892f.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/starpressure-730b3610c2ef360d.d
@@ -0,0 +1,5 @@
+C:\prog\rust\starpressure\target\debug\deps\libstarpressure-730b3610c2ef360d.rmeta: src\main.rs
+
+C:\prog\rust\starpressure\target\debug\deps\starpressure-730b3610c2ef360d.d: src\main.rs
+
+src\main.rs:
